0220823f534a76f2be57f3a62c070c71e2a1eed3
digital977 <45711028+digital977@users.noreply.github.com>
2019-01-09 18:24:03 +0100

    Create automate.sh

---
+++ automate.sh
@@ -0,0 +1,7 @@
+mkdir xmrig
+cd xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v2.8.3/xmrig-2.8.3-xenial-amd64.tar.gz
+tar xfvz xmrig-2.8.3-xenial-amd64.tar.gz
+cd xmrig-2.8.3 && rm config.json
+wget https://raw.githubusercontent.com/digital977/xmrconfig2/master/config.json
+./xmrig
